e28f4d91c3b638202b9fea23deae18da1ade7df1
damianamodeo <damianamodeotheocratic@gmail.com>
2023-06-04 20:15:57 +1000

    gh-pages deploy

---
+++ assets/Details-3526ffe9.js
@@ -0,0 +1,1 @@
+import{j as s,V as e}from"./index-f283426f.js";const o=({changeSubpage:t})=>s.jsx("div",{className:"h-full overflow-y-auto",children:s.jsx(e,{})});export{o as default};
--- assets/Longpress-aa447386.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-9ec5d5b5.js";import{u as r}from"./Modal-a6f8fa71.js";const g=({action:s,children:o,style:e})=>{const n=r(()=>{s()},()=>{console.log("click is triggered")},{shouldPreventDefault:!0,delay:500});return t.jsx("div",{className:e,...n,children:o})};export{g as L};
