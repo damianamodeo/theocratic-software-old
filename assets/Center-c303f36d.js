705da52718cacea63a5778ab6ac501cd2422c38e
damianamodeo <damianamodeotheocratic@gmail.com>
2023-06-04 16:06:39 +1000

    gh-pages deploy

---
+++ assets/Center-c303f36d.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-7b6c473b.js";const m=({children:e,style:t,vertically:r})=>s.jsx("div",{className:`grid place-items-center ${t} ${r&&"m-auto"} `,children:e});export{m as C};
