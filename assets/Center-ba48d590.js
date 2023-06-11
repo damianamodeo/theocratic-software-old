55fff1e216a813039f713418a1f4109d0acbb27d
damianamodeo <damianamodeotheocratic@gmail.com>
2023-06-11 15:00:00 +1000

    gh-pages deploy

---
+++ assets/Center-ba48d590.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-e730a459.js";const m=({children:e,style:t,vertically:r})=>s.jsx("div",{className:`grid place-items-center ${t} ${r&&"m-auto"} `,children:e});export{m as C};
--- assets/Longpress-69b7caf8.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-347aa002.js";import{u as r}from"./Modal-00fbf30e.js";const g=({action:s,children:o,style:e})=>{const n=r(()=>{s()},()=>{console.log("click is triggered")},{shouldPreventDefault:!0,delay:500});return t.jsx("div",{className:e,...n,children:o})};export{g as L};
