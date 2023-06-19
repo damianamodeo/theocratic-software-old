c996d47ce41a9cf95005d558067bb3002338e912
damianamodeo <damianamodeotheocratic@gmail.com>
2023-06-19 20:38:36 +1000

    gh-pages deploy

--- assets/Longpress-8367b5c0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-2a3da83f.js";import{u as r}from"./Modal-b247ee69.js";const g=({action:s,children:o,style:e})=>{const n=r(()=>{s()},()=>{console.log("click is triggered")},{shouldPreventDefault:!0,delay:500});return t.jsx("div",{className:e,...n,children:o})};export{g as L};
