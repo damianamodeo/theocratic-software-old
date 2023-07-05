b1c90a0d7d125f2c39408cb8b592cda7146c185f
damianamodeo <damianamodeotheocratic@gmail.com>
2023-07-05 14:59:36 +1000

    take 4

--- src/apps/orderly/pages/home/home/Home.tsx
+++ src/apps/orderly/pages/home/home/Home.tsx
@@ -1,5 +1,5 @@
 import AddMapID from "@COMPONENTS/firebase/AddMaps";
-// import Button from "@INPUTS/button/Button";
+import Button from "@INPUTS/Button";
 import { useEffect } from "react";
 
 type HomeType = {
@@ -10,16 +10,16 @@ const Home = ({ changeSubpage }: HomeType) => {
   return (
     <div className={`grid place-items-center gap-6`}>
       <AddMapID></AddMapID>
-      <Button clickAction={() => changeSubpage("Add Publisher", "<")}>
+      <Button color={"blue"} width={"md"} clickAction={() => changeSubpage("Add Publisher", "<")}>
         Add Publisher
       </Button>
-      <Button clickAction={() => changeSubpage("Publishers List", "<")}>
+      <Button color={"blue"} width={"md"} clickAction={() => changeSubpage("Publishers List", "<")}>
         Publishers List
       </Button>
-      <Button clickAction={() => changeSubpage("Update Publisher", "<")}>
+      <Button color={"blue"} width={"md"} clickAction={() => changeSubpage("Update Publisher", "<")}>
         Update Publisher
       </Button>
-      <Button clickAction={() => changeSubpage("Test", "<")}>Test Page</Button>
+      <Button color={"blue"} width={"md"} clickAction={() => changeSubpage("Test", "<")}>Test Page</Button>
     </div>
   );
 };
